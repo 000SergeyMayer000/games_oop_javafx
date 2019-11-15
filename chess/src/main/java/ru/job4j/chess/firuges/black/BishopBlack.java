f746a5b2f83f5793a116652713f8ac8aefe07957
Sergey Mayer <samajor@mail.ru>
2019-11-15 21:53:54 +0300

    add BishopBlackTest

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -4,7 +4,6 @@ import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
 /**
- *
  * @author Petr Arsentev (parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
@@ -23,7 +22,24 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        return new Cell[] { dest };
+        if (!isDiagonal(source, dest)) {
+            throw new IllegalStateException(
+                    String.format("Could not move by diagonal from %s to %s", source, dest)
+            );
+        }
+        int size = dest.x-source.x;
+        Cell[] steps = new Cell[size];
+        int deltaX = dest.x - source.x;
+        int deltaY = dest.y - source.y;
+        for (int index = 0; index < size; index++) {
+            steps[index] =  new Cell(1 , 2);
+        }
+        return steps;
+    }
+
+    public boolean isDiagonal(Cell source, Cell dest) {
+        //TODO check diagonal
+        return false;
     }
 
     @Override
